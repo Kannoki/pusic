c4dd3851d51093b72bd5843b10bc98a90cba76aa
Kannoki <75610750+Kannoki@users.noreply.github.com>
2022-05-27 18:11:49 +0700

    delete something, maybe :D

--- src/components/PlayerButton.js
+++ src/components/PlayerButton.js
@@ -1,34 +1,47 @@
 import React from 'react';
 import { AntDesign } from '@expo/vector-icons';
 import color from '../misc/color';
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 
 const PlayerButton = props => {
 
-  const { iconType, size = 40, iconColor = color.FONT, onPress } = props;
+    const { iconType, size = 40, iconColor = color.FONT, onPress } = props;
 
-  
-  const getIconName = type => {
-    switch (type) {
-      case 'PLAY':
-        return 'pausecircle';
-      case 'PAUSE':
-        return 'playcircleo';
-      case 'NEXT':
-        return 'forward';
-      case 'PREV':
-        return 'banckward';
-    }
-  };
-  return (
-    <AntDesign
-      {...props}
-      onPress={onPress}
-      name={getIconName(iconType)}
-      size={size}
-      color={iconColor}
-    />
-  );
+
+    const getIconName = type => {
+        switch (type) {
+            case 'PLAY':
+                return 'pausecircle';
+            case 'PAUSE':
+                return 'playcircleo';
+            case 'NEXT':
+                return 'forward';
+            case 'PREV':
+                return 'banckward';
+            case 'SHUF':
+                return 'fa-solid fa-shuffle';
+            case 'FLOW':
+                return 'double-arrow';
+            case 'LOOP':
+                return 'loop';
+            case 'CONT':
+                return 
+            
+
+        }
+    };
+    return (
+        <AntDesign
+            {...props}
+            onPress={onPress}
+            name={getIconName(iconType)}
+            size={size}
+            color={iconColor}
+        />
+    );
 };
 
 export default PlayerButton;
